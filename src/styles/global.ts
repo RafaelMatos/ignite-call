709531c2dd41e395c0f31752c924d6d86de55d31
RafaelMatos <rafael-matos@outlook.com>
2023-11-16 16:31:18 -0300

    global : added style to scroll bar

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -11,4 +11,20 @@ export const globalStyles = globalCss({
     color: '$gray100',
     '--webkit-font-smoothing': 'antialiased',
   },
+
+  /* Chrome, Edge, and Safari */
+  ' *::-webkit-scrollbar': {
+    width: '6px',
+  },
+
+  '*::-webkit-scrollbar-track': {
+    background: 'transparent',
+    ' border-radius': '5px',
+  },
+
+  '*::-webkit-scrollbar-thumb': {
+    'background-color': '$ignite500',
+    'border-radius': '14px',
+    opacity: '0.5',
+  },
 })
